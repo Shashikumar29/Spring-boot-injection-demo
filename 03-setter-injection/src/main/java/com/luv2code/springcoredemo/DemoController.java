8c34420bdd87b20bc925b64f5e6537795ba5d2bd
ShashikumarNV <sharkshashi1999@gmail.com>
2025-09-04 18:55:26 +0530

    implemented setter injection

---
+++ 03-setter-injection/src/main/java/com/luv2code/springcoredemo/DemoController.java
@@ -0,0 +1,20 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private Coach myCoach;
+
+    @Autowired
+    public void setMyCoach(Coach thisCoach) {
+        myCoach = thisCoach;
+    }
+
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkOut() {
+        return myCoach.getDailyWorkout();
+    }
+}
